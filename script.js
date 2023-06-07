9c3e50df1af4fb83ebb39ba3902575e453e9a509
Nazam <nazamahmad022@gmail.com>
2023-06-07 12:23:29 +0545

    Second Commit

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
     taskbar.addEventListener("click",()=>{
         console.log("clicked")
          if(startmenu.style.bottom == "50px"){
-             startmenu.style.bottom = "-647px"
+             startmenu.style.bottom = "-648px"
          }
          else{
              startmenu.style.bottom = "50px"
